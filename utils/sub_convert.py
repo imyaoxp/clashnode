32eeb0bb120042d52f11a65098c921fedf01c318
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2025-06-07 14:18:46 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -1071,46 +1071,47 @@ class sub_convert():
                 
                 
                 elif proxy['type'] == 'ss' : # SS 节点提取, 由 ss_base64_decoded 部分(参数: 'cipher', 'password', 'server', 'port') Base64 编码后 加 # 加注释(URL_encode) 
-                    if 'plugin' not in proxy :
-                        ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
-                        ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
-                        ss_proxy = str('ss://' + ss_base64 + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
+                    try:
+                        if 'plugin' not in proxy :
+                            ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
+                            ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
+                            ss_proxy = str('ss://' + ss_base64 + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
                         
-                    elif proxy['plugin'] == 'obfs' :
-                        #print(proxy)
-                        if 'mode' not in proxy['plugin-opts']:
-                            proxy['plugin-opts']['mode']='http'
-                        if 'host' not in proxy['plugin-opts']:
-                            proxy['plugin-opts']['host']=proxy['server']
+                        elif proxy['plugin'] == 'obfs' :
+                            #print(proxy)
+                            if 'mode' not in proxy['plugin-opts']:
+                                proxy['plugin-opts']['mode']='http'
+                            if 'host' not in proxy['plugin-opts']:
+                                proxy['plugin-opts']['host']=proxy['server']
                         
                             
-                        ssplugin=str('obfs='+str(proxy['plugin-opts']['mode']) + ';' + 'obfs-host=' + str(proxy['plugin-opts']['host']))
-                        #print(ssplugin)
-                        ssplugin=str(urllib.parse.quote(ssplugin))
-                        ss_base64_decoded = str(str(proxy['cipher']) + ':' + str(proxy['password']))
-                        ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
-                        ss_base64 = str(ss_base64+ '@' + str(proxy['server']) + ':' + str(proxy['port']))
-                        ss_proxy = str('ss://' + ss_base64 +  '/?plugin=obfs-local%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
-                        #print(ss_proxy)
-                    elif proxy['plugin'] == 'v2ray-plugin' :
-                        if 'mode' not in proxy['plugin-opts']:
-                            proxy['plugin-opts']['mode']='websocket'
-                        if 'host' not in proxy['plugin-opts']:
-                            proxy['plugin-opts']['host']=proxy['server']
-                        if 'path' not in proxy['plugin-opts']:
-                            proxy['plugin-opts']['path']='/'
-                        #print(proxy)
-                        ssplugin=str('mode='+str(proxy['plugin-opts']['mode']) + ';' + 'host=' + str(proxy['plugin-opts']['host'])+ ';' + 'path=' + str(proxy['plugin-opts']['path'])+';'+'tls;'+'mux=4;'+'mux=mux=4;')
-                        #print(ssplugin)
-                        ssplugin=str(urllib.parse.quote(ssplugin))
-                        ss_base64_decoded = str(str(proxy['cipher']) + ':' + str(proxy['password']))
-                        ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
-                        ss_base64 = str(ss_base64+ '@' + str(proxy['server']) + ':' + str(proxy['port']))
-                        ss_proxy = str('ss://' + ss_base64 +  '/?plugin=v2ray-plugin%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
-                        #print(ss_proxy)
+                            ssplugin=str('obfs='+str(proxy['plugin-opts']['mode']) + ';' + 'obfs-host=' + str(proxy['plugin-opts']['host']))
+                            #print(ssplugin)
+                            ssplugin=str(urllib.parse.quote(ssplugin))
+                            ss_base64_decoded = str(str(proxy['cipher']) + ':' + str(proxy['password']))
+                            ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
+                            ss_base64 = str(ss_base64+ '@' + str(proxy['server']) + ':' + str(proxy['port']))
+                            ss_proxy = str('ss://' + ss_base64 +  '/?plugin=obfs-local%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
+                            #print(ss_proxy)
+                        elif proxy['plugin'] == 'v2ray-plugin' :
+                            if 'mode' not in proxy['plugin-opts']:
+                                proxy['plugin-opts']['mode']='websocket'
+                            if 'host' not in proxy['plugin-opts']:
+                                proxy['plugin-opts']['host']=proxy['server']
+                            if 'path' not in proxy['plugin-opts']:
+                                proxy['plugin-opts']['path']='/'
+                            #print(proxy)
+                            ssplugin=str('mode='+str(proxy['plugin-opts']['mode']) + ';' + 'host=' + str(proxy['plugin-opts']['host'])+ ';' + 'path=' + str(proxy['plugin-opts']['path'])+';'+'tls;'+'mux=4;'+'mux=mux=4;')
+                            #print(ssplugin)
+                            ssplugin=str(urllib.parse.quote(ssplugin))
+                            ss_base64_decoded = str(str(proxy['cipher']) + ':' + str(proxy['password']))
+                            ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
+                            ss_base64 = str(ss_base64+ '@' + str(proxy['server']) + ':' + str(proxy['port']))
+                            ss_proxy = str('ss://' + ss_base64 +  '/?plugin=v2ray-plugin%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
+                            #print(ss_proxy)
 
                     
-                    protocol_url.append(ss_proxy)
+                        protocol_url.append(ss_proxy)
 
                     except Exception as err:
                         print(f'SS生成错误: {err} | 节点: {proxy.get("name", "未知")}')
