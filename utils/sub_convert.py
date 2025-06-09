327533c6c4013f1ced3519a4f4a48890ef0d7fb3
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2025-06-09 17:33:15 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -591,7 +591,7 @@ class sub_convert():
                     security_type = get_param_priority('security', 'Security', default='none').lower()
                     if security_type == 'reality':
                         yaml_node['reality-opts'] = {
-                            'public-key': get_param_priority('pbk', 'PublicKey', 'publicKey'）or "",
+                            'public-key': get_param_priority('pbk', 'PublicKey', 'publicKey') or "",
                             'short-id': get_param_priority('sid', 'ShortId', 'shortId') or "" 
                         }
                         flow = get_param_priority('flow', 'Flow', default='')
