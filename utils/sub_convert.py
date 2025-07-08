4f5947dfdfa93d605ec83817cc8120073de5866d
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2025-07-08 23:45:49 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -753,19 +753,19 @@ class sub_convert():
                             server
                         )
                         yaml_node['ws-opts'] = {
-                            'path': decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/')),
+                            'path': sub_convert.decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/')),
                             'headers': {'Host': ws_host}
                         }
                 
                     elif network_type == 'httpupgrade' or network_type == 'http' or network_type == 'xhttp' :
                         params = {}
                         http_opts = yaml_node.get('http-opts', {})
-                        path = decode_url_path(http_opts.get('path', ''))
+                        path = sub_convert.decode_url_path(http_opts.get('path', ''))
                         #if path.count('@') >1 or path.count('%40') >1:
                         #    print(f'vless节点格式错误，line:{line}')
                         #    continue
                         params['type'] = 'httpupgrade'
-                        params['path'] = decode_url_path(http_opts.get('path', '/'))
+                        params['path'] = sub_convert.decode_url_path(http_opts.get('path', '/'))
                         if 'host' in http_opts.get('headers', {}):
                             params['host'] = http_opts['headers']['host']
                         elif 'sni' in yaml_node:
@@ -780,7 +780,7 @@ class sub_convert():
                     elif network_type == 'h2':
 
                         host=get_param_priority('host', 'Host', 'HOST', default='').split(',')
-                        path=decode_url_path(get_param_priority('path', 'Path', 'PATH', default=''))
+                        path=sub_convert.decode_url_path(get_param_priority('path', 'Path', 'PATH', default=''))
                         #if path.count('@') >1 or path.count('%40') >1:
                         #    print(f'vless节点格式错误，line:{line}')
                         #    continue                
@@ -791,7 +791,7 @@ class sub_convert():
                             if host:
                                 h2_opts['host'] = host
                             if path:
-                                h2_opts['path'] = decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/'))
+                                h2_opts['path'] = sub_convert.decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/'))
                             if h2_opts:  # 仅在 tcp_opts 非空时添加
                                 yaml_node['h2-opts'] = h2_opts
                         
@@ -800,7 +800,7 @@ class sub_convert():
                     elif network_type == 'tcp':
                         header_type = get_param_priority('headerType', 'headertype', default='')
                         host = get_param_priority('Host', 'host', 'HOST', default='')
-                        path = decode_url_path(get_param_priority('path', 'Path', 'PATH', default=''))
+                        path = sub_convert.decode_url_path(get_param_priority('path', 'Path', 'PATH', default=''))
                         #if path.count('@') >1 or path.count('%40') >1:
                         #    print(f'vless节点格式错误，line:{line}')
                         #    continue                       
@@ -809,7 +809,7 @@ class sub_convert():
                             if host:
                                 tcp_opts['headers'] = {'Host': host.split(',')}
                             if path:
-                                tcp_opts['path'] = decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/'))
+                                tcp_opts['path'] = sub_convert.decode_url_path(get_param_priority('path', 'Path', 'PATH', default='/'))
                             if tcp_opts:  # 仅在 tcp_opts 非空时添加
                                 yaml_node['tcp-opts'] = tcp_opts
                     else:
