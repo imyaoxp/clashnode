8caecbbe0c7b5926a3cc3caf3d9c172f074ec604
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2025-07-11 09:23:51 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -702,7 +702,7 @@ class sub_convert():
                     # 2. gRPC处理
                     elif network_type == 'grpc':
                         yaml_node['grpc-opts'] = {
-                            'grpc-service-name': sub_convert.decode_url_path(get_param_priority('serviceName', 'servicename', default='')).replace(':','%3A').replace(',', '%2C').replace('@', '%40')
+                            'grpc-service-name': sub_convert.decode_url_path(get_param_priority('serviceName', 'servicename', default='')).replace(':','%3A').replace(',', '%2C').replace('@', '')
                         }
 
                     # 3. HTTP/2处理
